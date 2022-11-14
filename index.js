127d70c1c0b35abbefa29eee0bd06656c33114c7
rishi-kumar <rishikumar10dec@gmail.com>
2022-11-14 17:39:48 +0530

    added game function

--- index.js
+++ index.js
@@ -17,18 +17,51 @@ const getComputerChoice = () => {
 
 const playRound = (user, comp) => {
     if (user === comp) {
-        console.log('Game Tied!');
+        console.log('Round Tied!');
+        return -1;
     }
     else if((user === 'rock' && comp === 'scissors') || (user === 'scissors' && comp === 'paper') || (user === 'paper' && comp === 'rock')){
-        console.log(`You Won! ${user} beats ${comp}`);
+        console.log(`You Won this round! ${user} beats ${comp}`);
+        return 1;
     }
     else{
-        console.log(`You Lost! ${comp} beats ${user}`);
+        console.log(`You Lost this round! ${comp} beats ${user}`);
+        return 0;
     }
 }
 
-let userChoice = prompt('What is your choice?');
-userChoice = userChoice.toLocaleLowerCase();
+const game = () => {
+    let user = 0;
+    let comp = 0;
 
-playRound(userChoice, getComputerChoice());
+    for(let index = 0; index < 5; index++){
+        let userChoice = prompt('What is your choice?');
+        userChoice = userChoice.toLocaleLowerCase();
+        compChoice = getComputerChoice();
+        let result = playRound(userChoice, compChoice);
+
+        if(result === 1){
+            user++;
+        }
+        else if(result === 0){
+            comp++;
+        }
+    }
+
+    if (user === comp) {
+        console.log('GAME TIED!!');
+    }
+    else if (user > comp){
+        console.log('You WON the game!!');
+    }
+    else{
+        console.log('You LOST the game!!');
+    }
+}
+
+game();
+
+
+
+
 
